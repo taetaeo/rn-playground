dc2e1b24517db44810c74932fa9c08a3163ad870
ohtaekwon <ohtk1119@gmail.com>
2024-01-23 13:25:03 +0900

    글자수 제한 맞추도록 프리티어 설정

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>RN Playground</Text>
+      <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }} />
     </View>
   );
 }
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  ///printWidth를 설정, json형식에서도 동일하다
+  printWidth: 180,
+};
